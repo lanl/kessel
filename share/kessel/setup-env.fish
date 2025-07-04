6932406d2825ac2eb9b349d0178bbe402320cdb5
Andrés Yagüe López <ayaguelopez@lanl.gov>
2025-07-03 19:03:15 -0600

    Improved temporary files security

--- share/kessel/setup-env.fish
+++ share/kessel/setup-env.fish
@@ -4,9 +4,17 @@ set -x KESSEL_CONFIG_DIR $KESSEL_ROOT/etc/kessel
 set -x IN_FISH true
 
 function kessel
-    eval (command kessel $argv 3>/tmp/kessel_commands >/tmp/kessel_output)
-    cat /tmp/kessel_output
-    source /tmp/kessel_commands
+    # Create safe temp files for the evaluation of kessel
+    set -f kessel_commands (mktemp -p $KESSEL_ROOT/share/kessel/.tmp)
+    set -f kessel_output (mktemp -p $KESSEL_ROOT/share/kessel/.tmp)
+
+    eval (command kessel $argv 3>$kessel_commands >$kessel_output)
+
+    cat $kessel_output
+    source $kessel_commands
+
+    rm $kessel_commands
+    rm $kessel_output
 end
 
 fish_add_path -p --path $KESSEL_ROOT/bin
