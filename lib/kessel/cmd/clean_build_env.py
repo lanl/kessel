661a9f8e0f4aba3887305cb32171ad14019262c8
Richard Berger <rberger@lanl.gov>
2025-07-08 09:24:06 -0600

    add clean-build-env helper command

---
+++ lib/kessel/cmd/clean_build_env.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+
+def clean_build_env(args, ctx, senv):
+    if args.env_file.exists():
+        tmp_file = f"{args.env_file}.tmp"
+        with open(args.env_file, "r") as r, open(tmp_file, "w") as w:
+            for line in r:
+                if not ( line.startswith("FLUX_") or line.startswith("SLURM_") ):
+                    print(line, file=w)
+        os.rename(tmp_file, args.env_file)
+
+def setup_command(subparser):
+    subparser.add_argument("env_file", type=Path)
+    subparser.set_defaults(func=clean_build_env)
