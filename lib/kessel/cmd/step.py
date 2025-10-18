63e34c1ffb8cf70c2e72b1d2884a8e9f63156be2
Richard Berger <rberger@lanl.gov>
2025-10-18 00:47:11 -0600

    let steps provide their own help

--- lib/kessel/cmd/step.py
+++ lib/kessel/cmd/step.py
@@ -6,18 +6,21 @@ def step(args, extra_args, ctx, senv):
     workflow = ctx.workflow_config
     st = next(s for s in workflow.steps if s.name == args.step)
 
-    senv.section_start(
-        st.name, st.title, collapsed=st.collapsed
-    )
-    senv.echo(status(ctx, st.name))
+    is_help = "-h" in extra_args or "--help" in extra_args
+    if not is_help:
+        senv.section_start(
+            st.name, st.title, collapsed=st.collapsed
+        )
+        senv.echo(status(ctx, st.name))
     ignored_args = len(sys.argv) - len(extra_args) - 1
     senv.eval(f"shift {ignored_args}")
     senv.eval(f"source {st.script} " + " ".join([f"\"{a}\"" for a in extra_args]))
-    senv.eval("ret=$?")
 
-    senv.section_end(st.name)
-    senv.eval("test $ret -eq 0 && ", end="")
-    ctx.run_state = st.name
+    if not is_help:
+        senv.eval("ret=$?")
+        senv.section_end(st.name)
+        senv.eval("test $ret -eq 0 && ", end="")
+        ctx.run_state = st.name
 
 
 def setup_command(subparser, ctx):
@@ -28,5 +31,5 @@ def setup_command(subparser, ctx):
 
         for s in workflow.steps:
             name = s.name
-            step_cmd = subparsers.add_parser(name)
+            step_cmd = subparsers.add_parser(name, add_help=False)
             step_cmd.set_defaults(func=step, step=name)
