588e01f0f9c0a55a38c3d0bfb8cf01d5a3d156bd
Richard Berger <rberger@lanl.gov>
2025-12-13 01:47:34 -0700

    speed up finalize

--- libexec/kessel/workflows/spack_deployment/finalize.sh
+++ libexec/kessel/workflows/spack_deployment/finalize.sh
@@ -10,9 +10,18 @@ fi
 
 spack clean -a
 
-echo "Setting permissions and group..."
-chown -R ":$KESSEL_GROUP" "$KESSEL_DEPLOYMENT"
-chmod -R "$KESSEL_PERMISSIONS" "$KESSEL_DEPLOYMENT"
+echo "Setting permissions and group (excluding spack/opt/spack) ..."
+find "$KESSEL_DEPLOYMENT" -path "$KESSEL_DEPLOYMENT/spack/opt/spack" -prune -o -exec chown ":$KESSEL_GROUP" {} +
+find "$KESSEL_DEPLOYMENT" -path "$KESSEL_DEPLOYMENT/spack/opt/spack" -prune -o -type d -exec chmod "$KESSEL_PERMISSIONS" {} +
+find "$KESSEL_DEPLOYMENT" -path "$KESSEL_DEPLOYMENT/spack/opt/spack" -prune -o -type f -exec chmod "$KESSEL_PERMISSIONS" {} +
+
+find "$KESSEL_DEPLOYMENT/spack/opt/spack/bin" -o -exec chown ":$KESSEL_GROUP" {} +
+find "$KESSEL_DEPLOYMENT/spack/opt/spack/bin" -o -type d -exec chmod "$KESSEL_PERMISSIONS" {} +
+find "$KESSEL_DEPLOYMENT/spack/opt/spack/bin" -o -type f -exec chmod "$KESSEL_PERMISSIONS" {} +
+
+find "$KESSEL_DEPLOYMENT/spack/opt/spack/.spack-db" -o -exec chown ":$KESSEL_GROUP" {} +
+find "$KESSEL_DEPLOYMENT/spack/opt/spack/.spack-db" -o -type d -exec chmod "$KESSEL_PERMISSIONS" {} +
+find "$KESSEL_DEPLOYMENT/spack/opt/spack/.spack-db" -o -type f -exec chmod "$KESSEL_PERMISSIONS" {} +
 
 chmod g+s "$KESSEL_DEPLOYMENT/spack/opt/spack/.spack-db"
 
@@ -20,3 +29,8 @@ SQFS_FILE="$KESSEL_DEPLOYMENT/.replicate.sqfs"
 "$KESSEL_ROOT/libexec/kessel/workflows/spack_deployment/gen_replicate_sqfs" "$KESSEL_DEPLOYMENT" "$SQFS_FILE"
 chown ":$KESSEL_GROUP" "$SQFS_FILE"
 chmod "$KESSEL_PERMISSIONS" "$SQFS_FILE"
+
+echo "Setting permissions and group of spack/opt/spack ..."
+chown -R ":$KESSEL_GROUP" "$KESSEL_DEPLOYMENT/spack/opt/spack"
+chmod -R "$KESSEL_PERMISSIONS" "$KESSEL_DEPLOYMENT/spack/opt/spack"
+chmod g+s "$KESSEL_DEPLOYMENT/spack/opt/spack/.spack-db"
