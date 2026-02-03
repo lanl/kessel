90852136084ef951ffc7015623200652ad5d5760
Andrés Yagüe López <and.yague@gmail.com>
2026-02-02 17:00:36 -0700

    Use the autopep8 and flake8 options on project root

--- .kessel/workflows/format/autopep8.sh
+++ .kessel/workflows/format/autopep8.sh
@@ -1,4 +1,4 @@
 (
 source "$KESSEL_BUILD_ENV"
-python3 -m autopep8 -iaar --max-line-length 120 "$KESSEL_SOURCE_DIR/lib"
+python3 -m autopep8 -iaar "$KESSEL_SOURCE_DIR/lib"
 )
--- .kessel/workflows/format/flake8.sh
+++ .kessel/workflows/format/flake8.sh
@@ -1,4 +1,4 @@
 (
 source "$KESSEL_BUILD_ENV"
-python3 -m flake8 --max-line-length 120 --ignore=F401,E226,W503,W504 "$KESSEL_SOURCE_DIR/lib"
+python3 -m flake8 "$KESSEL_SOURCE_DIR/lib"
 )
