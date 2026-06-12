718e38b06c67e9877a38ef02b3138026778f51f1
Richard Berger <rberger@lanl.gov>
2026-06-12 10:13:54 -0600

    Only update user build cache index when requested
    Signed-off-by: Richard Berger <rberger@lanl.gov>

--- lib/kessel/workflows/base/spack/build_environment/prepare_env.sh
+++ lib/kessel/workflows/base/spack/build_environment/prepare_env.sh
@@ -68,5 +68,7 @@ spack config add "packages:${KESSEL_PROJECT_NAME}:package_attributes:keep_werror
 if [ -n "${KESSEL_USER_BUILD_CACHE}" ]; then
   mkdir -p "${KESSEL_USER_BUILD_CACHE}"
   spack mirror add --type binary --autopush --unsigned user-ci-mirror "${KESSEL_USER_BUILD_CACHE}"
-  spack buildcache update-index "${KESSEL_USER_BUILD_CACHE}" || true
+  if [ "${KESSEL_USER_BUILD_CACHE_UPDATE_INDEX}" ]; then
+    spack buildcache update-index "${KESSEL_USER_BUILD_CACHE}" || true
+  fi
 fi
